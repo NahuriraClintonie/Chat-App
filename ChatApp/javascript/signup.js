5ed82b7418c69cca324ed521ab23355d0f8a0ee9
NahuriraClintonie <clintonnahurira24@gmail.com>
2025-11-17 11:37:35 +0300

    data: fixed the image upload storage as a blob

--- ChatApp/javascript/signup.js
+++ ChatApp/javascript/signup.js
@@ -7,21 +7,34 @@ form.onsubmit = (e)=>{
 }
 
 continueBtn.onclick = ()=>{
+    console.log("Signup button clicked!");
+
     let xhr = new XMLHttpRequest();
     xhr.open("POST", "php/signup.php", true);
+
     xhr.onload = ()=>{
+      console.log("Request finished. Status:", xhr.status);
+
       if(xhr.readyState === XMLHttpRequest.DONE){
           if(xhr.status === 200){
               let data = xhr.response;
-              if(data === "success"){
+              console.log("Server response:", data);
+
+              if(data.trim() === "success"){
+                console.log("Redirecting to users.php...");
                 location.href="users.php";
               }else{
                 errorText.style.display = "block";
                 errorText.textContent = data;
+                console.log("Error displayed:", data);
               }
+          } else {
+            console.error("Server returned non-200 status", xhr.status);
           }
       }
     }
+
     let formData = new FormData(form);
+    console.log("Sending FormData:", [...formData.entries()]);
     xhr.send(formData);
 }
