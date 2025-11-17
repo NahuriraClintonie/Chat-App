73720bfcc965bf1da0a1bd3424387b8cab5b9a47
NahuriraClintonie <clintonnahurira24@gmail.com>
2025-11-17 12:00:28 +0300

    logic: fix blob retrieval and display on the users page

--- ChatApp/php/users.php
+++ ChatApp/php/users.php
@@ -1,14 +1,23 @@
 <?php
-    session_start();
-    include_once "config.php";
-    $outgoing_id = $_SESSION['unique_id'];
-    $sql = "SELECT * FROM users WHERE NOT unique_id = {$outgoing_id} ORDER BY user_id DESC";
-    $query = mysqli_query($conn, $sql);
-    $output = "";
-    if(mysqli_num_rows($query) == 0){
-        $output .= "No users are available to chat";
-    }elseif(mysqli_num_rows($query) > 0){
-        include_once "data.php";
-    }
-    echo $output;
+session_start();
+include_once "config.php";
+
+// redirect if not logged in
+if(!isset($_SESSION['unique_id'])){
+    header("Location: login.php");
+    exit;
+}
+
+$outgoing_id = $_SESSION['unique_id'];
+$sql = "SELECT * FROM users WHERE NOT unique_id = {$outgoing_id} ORDER BY user_id DESC";
+$query = mysqli_query($conn, $sql);
+$output = "";
+
+if($query && mysqli_num_rows($query) > 0){
+    include_once "data.php";
+} else {
+    $output .= "No users are available to chat";
+}
+
+echo $output;
 ?>
